feace95aff1ef74cc6fa290461e08aa3c41480f9
vengineer-systemverilog <vengineer@LAPTOP-HGGR6RPK.localdomain>
2022-05-12 09:14:53 +0900

    add Xilinx xsim

---
+++ xsim/sample_xsi_bfm_1/test_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+#include "bfm.h"
+
+void bfm::test_main(){
+    cout << "start test_main" << endl;
+    write(0x200, 0x12345678);
+    uint32_t data = read(0x200);
+    if(data != 0x12345678)
+        cout << "<<ERR>>, compare error, data = 0x" << hex << data << endl;
+    cout << "finish test_main" << endl;
+}
